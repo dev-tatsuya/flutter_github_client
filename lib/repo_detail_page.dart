1a8bbfc723f0c363a18870dbbebde4fc8c168cb0
dev-tatsuya <development.tatsuya@gmail.com>
2024-09-06 01:34:01 +0900

    update: REST のリポジトリ詳細 UI

--- lib/repo_detail_page.dart
+++ lib/repo_detail_page.dart
@@ -1,8 +1,12 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_github_client/api_protocol_state.dart';
+import 'package:flutter_github_client/graphql/graphql_container.dart';
 import 'package:flutter_github_client/graphql/repo_detail_query.graphql.dart';
 import 'package:flutter_github_client/model.dart';
 import 'package:flutter_github_client/repo_list_page.dart';
+import 'package:flutter_github_client/rest/repo_state.dart';
+import 'package:flutter_github_client/rest/rest_container.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
@@ -18,27 +22,19 @@ class RepoDetailPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final query = useQuery$RepoDetail(
+    final apiProtocol = ref.watch(apiProtocolStateProvider);
+    final graphqlResult = useQuery$RepoDetail(
       Options$Query$RepoDetail(
         variables: Variables$Query$RepoDetail(
           owner: owner,
           name: name,
         ),
       ),
-    );
-    final result = query.result;
-    final data = result.parsedData?.repository;
+    ).result;
+    final restResult = ref.watch(repoDetailProvider(owner: owner, repo: name));
 
-    final Widget child;
-    if (result.hasException) {
-      child = Center(child: Text('${result.exception}'));
-    } else if (result.isLoading) {
-      child = const Center(child: Text('Fetching ...'));
-    } else if (data == null) {
-      child = const Center(child: Text('Empty'));
-    } else {
-      final item = Repository.fromGraphQL(data);
-      child = Column(
+    Widget builder(Repository item) {
+      return Column(
         children: [
           RepoListItem(item: item, isUsedOnDetail: true),
           Container(
@@ -50,9 +46,25 @@ class RepoDetailPage extends HookConsumerWidget {
       );
     }
 
+    final graphQLContainer = GraphQLContainer(
+      result: graphqlResult,
+      builder: (data) {
+        if (data.repository == null) return null;
+        return builder(Repository.fromGraphQL(data.repository!));
+      },
+    );
+
+    final restContainer = RestContainer(
+      result: restResult,
+      builder: builder,
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text(name)),
-      body: child,
+      body: switch (apiProtocol) {
+        ApiProtocolType.graphql => graphQLContainer,
+        ApiProtocolType.rest => restContainer,
+      },
     );
   }
 }
